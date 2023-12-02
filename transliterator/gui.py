8106d8722e7ae0e39f847ba6109958474f73c328
Deyan Nikolov <deyan.nikolov@gmail.com>
2023-12-02 08:04:25 +0200

    Add theme chooser and about section.

--- transliterator/gui.py
+++ transliterator/gui.py
@@ -2,26 +2,34 @@
 A GUI for the transliterator based on PySimpleGUI.
 """
 import os
-from trans import transliterate
 import PySimpleGUI as sg
+
+from trans import transliterate
+
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 sg.SetOptions (
-    font = ("Helvetica", 12, "bold"),
+    font = ("Helvetica", 11),
     element_padding = (8, 8),
 )
 
 WIN_WIDTH = 950
 WIN_HEIGHT = 800
 
-def launch_gui():
-    layout = [[sg.Text('Place text to transliterate here')],
-              [sg.Multiline(size=(80,15), key="-BG TEXT-", autoscroll=True)],
-              [sg.Multiline(size=(80,15), key="-ENG TEXT-", autoscroll=True)],
-              [sg.Button("Transliterate"), sg.Button("Exit")]]
+def make_window():
+    sg.theme(sg.user_settings_get_entry("theme", None))
+    layout = [
+        [sg.Menu([["File", ["Theme", "Exit"]], ["Help", "About"],])],
+        [sg.Text("Transliterator")],
+        [sg.Multiline(size=(80,15), key="-BG TEXT-", autoscroll=True)],
+        [sg.Multiline(size=(80,15), key="-EN TEXT-", autoscroll=True)],
+        [sg.Button("Transliterate"), sg.Button("Clear"), sg.Button("Exit")],
+    ]
+    return sg.Window("Transliterator", layout, keep_on_top=True, finalize=True)
 
-    window = sg.Window("Transliterator", layout, size=(WIN_WIDTH, WIN_HEIGHT), keep_on_top=True, finalize=True)
+def main():
+    window = make_window()
 
     while True: 
         event, values = window.read()
@@ -29,7 +37,6 @@ def launch_gui():
 
         if event == sg.WIN_CLOSED or event == "Exit":
             break
-
         if event == "Transliterate":
             cyrillic_text = values["-BG TEXT-"]
             transliterated_lines = []
@@ -37,9 +44,25 @@ def launch_gui():
                 transliterated_line = transliterate(line)
                 transliterated_lines.append(transliterated_line)
             transliterated_text = "\n".join(transliterated_lines)
-            window['-ENG TEXT-'].update(transliterated_text)
+            window["-EN TEXT-"].update(transliterated_text)
+        if event == "Clear":
+            window["-EN TEXT-"].update("")
+            window["-BG TEXT-"].update("")
+        if event == "Theme":
+            ev, vals = sg.Window("Choose Theme", [[sg.Combo(sg.theme_list(), k="-THEME LIST-"), sg.OK(), sg.Cancel()]]).read(close=True)
+            if ev == "OK":
+                window.close()
+                sg.user_settings_set_entry("theme", vals["-THEME LIST-"])
+                window = make_window()
+        if event == "About":
+            ev, vals = sg.Window("About", [[sg.Text("About")],[sg.OK()]]).read(close=True)
+            if ev == "OK":
+                window.close()
+                window = make_window()
 
     window.close()
 
-if __name__ == '__main__':
-    launch_gui()
+
+if __name__ == "__main__":
+    sg.user_settings_filename(filename="settings.json")
+    main()
